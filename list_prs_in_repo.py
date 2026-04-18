6f9a698cae97893aad8e1578ef63ae8fd6e36db7
Murali Elavarthi <150920503+muralielavarthi@users.noreply.github.com>
2026-04-18 15:18:34 +0000

    c

--- list_prs_in_repo.py
+++ list_prs_in_repo.py
@@ -1,10 +1,12 @@
+# We will fetch list of Open Pull Requests
+
 import requests
 
 def get_PR_authors(item):
         response=requests.get(item)
         if response.status_code == 200:
              data_dict=response.json() # parses JSON into Dict
-             count_prs(data_dict)
+             return count_prs(data_dict)
         else:
             print(f"unable to fetch data",response.status_code)
 
@@ -16,12 +18,14 @@ def count_prs(data):
             creators_data[creator]+=1
         else:
             creators_data[creator]=1
-    print(creators_data)
+    return creators_data
 
     
 owner_name=input("Enter a GitHub owner name:")
 repo_name=input("Enter a Repo name:")
 
-pr_list=get_PR_authors(f"https://api.github.com/repos/{owner_name}/{repo_name}/pulls")
+final_result=get_PR_authors(f"https://api.github.com/repos/{owner_name}/{repo_name}/pulls?state=open")
+print(final_result)
 
-print(pr_list)
+#By default, GitHub API returns only 30 pull requests per page.
+#If you want all PRs, please handle pagination
