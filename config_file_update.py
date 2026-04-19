2c03992ccac40b39cc21faabc3907741d0aad3d8
Murali Elavarthi <150920503+muralielavarthi@users.noreply.github.com>
2026-04-19 06:01:52 +0000

    c

---
+++ config_file_update.py
@@ -0,0 +1,11 @@
+def server_config_update(file_path,key,value):
+    with open(file_path,"r") as file:
+        lines=file.readlines()
+    with open(file_path,"w") as file:
+        for line in lines:
+            if key in line:
+                file.write(key+" "+"="+" "+value+"\n") #\n for text to come in new line
+            else:
+                file.write(line)
+
+server_config_update("server.config","PORT_NUMBER","22")
